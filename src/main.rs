694e7a6c37eb7810b7e51abf5d76546c8e2c4b8d
alek3y <alek3y@protonmail.com>
2022-06-25 00:37:37 +0200

    Change image crate to allow resizing

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@
 //
 // See: https://www.unicode.org/charts/PDF/U2800.pdf
 
-use std::{env, process, fs, slice};
-use stb_image_rust;
+use std::{env, process, fs};
+use image;
 
 const BRAILLE: u32 = 0x2800;
 
@@ -46,29 +46,15 @@ fn main() {
 		process::exit(1);
 	}
 
-	let mut image_bytes = fs::read(image_name.unwrap()).unwrap();
-	let mut width = 0i32;
-	let mut height = 0i32;
-	let mut channels = 0i32;
+	let image = image::load_from_memory(
+		&fs::read(image_name.unwrap()).unwrap()
+	).unwrap().into_luma8();
 
-	let image: &mut [u8];
-	unsafe {
-		let image_ptr = stb_image_rust::stbi_load_from_memory(
-			image_bytes.as_mut_ptr(),
-			image_bytes.len() as i32,
-			&mut width, &mut height,
-			&mut channels,
-			stb_image_rust::STBI_grey
-		);
-
-		image = slice::from_raw_parts_mut(image_ptr, (width*height) as usize);
-	}
-
-	println!("{}", image[0]);
-
-	unsafe {
-		stb_image_rust::stbi_image_free(image.as_mut_ptr());	// Oh boy, good ol' C
-	}
+	let image_stretched = image::imageops::resize(
+		&image,
+		image.width(), image.height() / 2,
+		image::imageops::FilterType::Triangle
+	);
 
 	//let character = char::from_u32(BRAILLE + 0b10010011).unwrap();
 	//println!("{:?}", character);
